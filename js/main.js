8135efc05a97709db751c0681cc98abead01ea0a
Karol Nowakowski <karol.a.nowakowski@gmail.com>
2018-09-20 17:04:41 +0200

    pierwszy

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var liczbaPierwsza = 12;
+var liczbaDruga = 7;
+function sum (liczbaPierwsza, liczbaDruga) {
+    var suma = liczbaPierwsza + liczbaDruga;
+    return suma;
+}
+
+console.log(sum(liczbaPierwsza, liczbaDruga));
